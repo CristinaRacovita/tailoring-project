ee715b65691002dce6c59b69db5bc6ef689f61f8
Cristina Racovita <cristina.racovita98@gmail.com>
2022-08-15 22:45:30 +0300

    add deployment settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
-	},
-	
+    kit: {
+        paths: { assets: "", base: "/tailoring-project" },
+        adapter: adapter({
+			pages: "docs",
+            assets: "docs",
+            fallback: 'index.html',
+            precompress: false,
+        })
+    }
 };
 
 export default config;
